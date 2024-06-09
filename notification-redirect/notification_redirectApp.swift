6d382036f826929f4ea42d38b98a63667e18949f
mrotante <michael.rotante@uconn.edu>
2024-06-09 15:03:01 -0400

    Initial Commit

---
+++ notification-redirect/notification_redirectApp.swift
@@ -0,0 +1,17 @@
+//
+//  notification_redirectApp.swift
+//  notification-redirect
+//
+//  Created by Michael Rotante on 6/9/24.
+//
+
+import SwiftUI
+
+@main
+struct notification_redirectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ notification-redirectUITests/notification_redirectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  notification_redirectUITestsLaunchTests.swift
+//  notification-redirectUITests
+//
+//  Created by Michael Rotante on 6/9/24.
+//
+
+import XCTest
+
+final class notification_redirectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
